2fdcc2adf148176d16762b4cd0b44c4bdaa39c3e
B-asile <84376166+B-asile@users.noreply.github.com>
2023-02-13 23:16:23 +0100

    test settings

--- project/oc_lettings_site/settings.py
+++ project/oc_lettings_site/settings.py
@@ -19,7 +19,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'oc_lettings_site.apps.OCLettingsSiteConfig',
+    #'oc_lettings_site.apps.OCLettingsSiteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'oc_lettings_site',
     'lettings',
     'profiles',
 ]
